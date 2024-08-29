9fa9a6f589666499e1c158a363e67daac918d2e5
yosi <yosihofman21@gmail.com>
2024-08-29 14:55:20 +0300

    login colors

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,6 +5,7 @@
 // import 'package:businesses/screens/signup_screen.dart';
 // import 'package:businesses/utils/utils.dart';
 import 'package:football/screens/signup_screen.dart';
+import 'package:football/theme/colors.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:football/models/users.dart';
@@ -37,10 +38,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   void initState() {
     super.initState();
-  
   }
-
-
 
   @override
   void dispose() {
@@ -54,29 +52,26 @@ class _LoginScreenState extends State<LoginScreen> {
       _isLoading = true;
     });
 
-     final authProvider = Provider.of<AuthProvider>(context, listen: false);
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
     authProvider.login(_usernameController.text, _passwordController.text);
 
     if (authProvider == 'success') {
-      await Provider.of<UserProvider >(context, listen: false)
+      await Provider.of<UserProvider>(context, listen: false)
           .refreshUser(_usernameController.text);
       if (context.mounted) {
         User? currentUser =
-            Provider.of<UserProvider>(context, listen: false)
-                .currentUser;
+            Provider.of<UserProvider>(context, listen: false).currentUser;
 
         if (currentUser != null) {
-     
-            // Navigate to admin screen
-            Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(
-                  builder: (context) => const ResponsiveLayout(
-                    mobileScreenLayout: MobileScreenLayout(),
-                    webScreenLayout: WebScreenLayout(),
-                  ),
+          // Navigate to admin screen
+          Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(
+                builder: (context) => const ResponsiveLayout(
+                  mobileScreenLayout: MobileScreenLayout(),
+                  webScreenLayout: WebScreenLayout(),
                 ),
-                (route) => false);
-
+              ),
+              (route) => false);
         }
       }
 
@@ -109,154 +104,209 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
-     final authProvider = Provider.of<AuthProvider>(context);
+    final authProvider = Provider.of<AuthProvider>(context);
     return Scaffold(
+        backgroundColor: background,
         body: SafeArea(
             child: Container(
-      padding: const EdgeInsets.symmetric(horizontal: 32),
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Flexible(
-            child: Container(),
-            flex: 2,
-          ),
-          //svg image
-          // SvgPicture.asset('assets/ic_instegram.svg', color: primaryColor,height:64),
-          const SizedBox(height: 64),
-          //test fiels input for email
-          Autocomplete<String>(
-            optionsBuilder: (TextEditingValue textEditingValue) {
-              if (textEditingValue.text == '') {
-                return const Iterable<String>.empty();
-              }
-              return _employeeUsernames.where((String option) {
-                return option
-                    .toLowerCase()
-                    .contains(textEditingValue.text.toLowerCase());
-              });
-            },
-            onSelected: (String selection) {
-              setState(() {
-                _usernameController.text = selection;
-              });
-            },
-            fieldViewBuilder: (BuildContext context,
-                TextEditingController fieldTextEditingController,
-                FocusNode fieldFocusNode,
-                VoidCallback onFieldSubmitted) {
-              return TextField(
-                controller: fieldTextEditingController,
-                focusNode: fieldFocusNode,
-                decoration: InputDecoration(
-                    labelText: 'user name'),
-                onChanged: (value) {
-                  _usernameController.text = value;
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+              //svg image
+              // SvgPicture.asset('assets/ic_instegram.svg', color: primaryColor,height:64),
+              const SizedBox(height: 64),
+              //test fiels input for email
+              Autocomplete<String>(
+                optionsBuilder: (TextEditingValue textEditingValue) {
+                  if (textEditingValue.text == '') {
+                    return const Iterable<String>.empty();
+                  }
+                  return _employeeUsernames.where((String option) {
+                    return option
+                        .toLowerCase()
+                        .contains(textEditingValue.text.toLowerCase());
+                  });
                 },
-              );
-            },
-            optionsViewBuilder: (BuildContext context,
-                AutocompleteOnSelected<String> onSelected,
-                Iterable<String> options) {
-              return Align(
-                alignment: Alignment.topLeft,
-                child: Material(
-                  elevation: 4.0,
-                  child: Container(
-                    width: 330,
-                    height: 60, // Adjust this width as needed
-                    child: ListView.builder(
-                      padding: EdgeInsets.all(8.0),
-                      itemCount: options.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        final String option = options.elementAt(index);
-                        return GestureDetector(
-                          onTap: () {
-                            onSelected(option);
+                onSelected: (String selection) {
+                  setState(() {
+                    _usernameController.text = selection;
+                  });
+                },
+                fieldViewBuilder: (BuildContext context,
+                    TextEditingController fieldTextEditingController,
+                    FocusNode fieldFocusNode,
+                    VoidCallback onFieldSubmitted) {
+                  return TextField(
+                    controller: fieldTextEditingController,
+                    focusNode: fieldFocusNode,
+                    decoration: InputDecoration(
+                      labelText: 'user name',
+                      labelStyle: TextStyle(
+                        color: Colors.blue, // Change this to your desired color
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors
+                                .blue), // Bottom border color when enabled
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.blue,
+                            width:
+                                2.0), // Bottom border color when focused, with thicker border
+                      ),
+                    ),
+                    style: TextStyle(
+                      color:
+                          Colors.white, // Change the input text color to blue
+                    ),
+                    cursorColor: Colors.blue,
+                    onChanged: (value) {
+                      _usernameController.text = value;
+                    },
+                  );
+                },
+                optionsViewBuilder: (BuildContext context,
+                    AutocompleteOnSelected<String> onSelected,
+                    Iterable<String> options) {
+                  return Align(
+                    alignment: Alignment.topLeft,
+                    child: Material(
+                      elevation: 4.0,
+                      child: Container(
+                        width: 330,
+                        height: 60, // Adjust this width as needed
+                        child: ListView.builder(
+                          padding: EdgeInsets.all(8.0),
+                          itemCount: options.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final String option = options.elementAt(index);
+                            return GestureDetector(
+                              onTap: () {
+                                onSelected(option);
+                              },
+                              child: ListTile(
+                                title: Text(option),
+                              ),
+                            );
                           },
-                          child: ListTile(
-                            title: Text(option),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+
+              const SizedBox(height: 24),
+              //old test fiels input for password
+              // TextFieldInput(
+              //   textEditingController: _passwordController,
+              //   hintText: AppLocalizations.of(context)!.enteryourpassword,
+              //   textInputType: TextInputType.text,
+              //   isPass: true,
+              // ),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'password',
+                  labelStyle: TextStyle(
+                    color: Colors.blue, // Change this to your desired color
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors.blue), // Bottom border color when enabled
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors.blue,
+                        width:
+                            2.0), // Bottom border color when focused, with thicker border
+                  ),
+                ),
+                style: TextStyle(
+                  color: Colors.white, // Change the input text color to blue
+                ),
+                cursorColor: Colors.blue,
+              ),
+              const SizedBox(height: 24),
+
+              InkWell(
+                onTap: authProvider.isLoading ? null : loginUser,
+                child: Container(
+                  child: authProvider.isLoading
+                      ? const Center(
+                          child: CircularProgressIndicator(
+                            color: primaryColor,
                           ),
-                        );
-                      },
+                        )
+                      : Text(
+                          'login',
+                          style: TextStyle(
+                            color: Colors
+                                .white, // Change the input text color to blue
+                          ),
+                        ),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                      ),
+                      color: blueColor),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: 
+          
+                             GestureDetector(
+                    onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const SignupScreen(),
+                      ),
+                    ),
+                    child: Container(
+                      child: Text(
+                        'signup',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 8),
                     ),
                   ),
-                ),
-              );
-            },
-          ),
-
-          const SizedBox(height: 24),
-          //old test fiels input for password
-          // TextFieldInput(
-          //   textEditingController: _passwordController,
-          //   hintText: AppLocalizations.of(context)!.enteryourpassword,
-          //   textInputType: TextInputType.text,
-          //   isPass: true,
-          // ),
-          TextField(
-            controller: _passwordController,
-            decoration: InputDecoration(
-                labelText: 'pass'),
-          ),
-          const SizedBox(height: 24),
-         
- 
-          InkWell(
-            onTap: authProvider.isLoading  ? null : loginUser,
-            child: Container(
-              child: authProvider.isLoading 
-                  ? const Center(
-                      child: CircularProgressIndicator(
-                        color: primaryColor,
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    
+                  ),
+                     Text(
+                      ' ?dont have account',
+                      style: TextStyle(
+                        color:
+                            Colors.white, // Change the input text color to blue
                       ),
-                    )
-                  : Text('login'),
-              width: double.infinity,
-              alignment: Alignment.center,
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              decoration: const ShapeDecoration(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(4)),
-                  ),
-                  color: blueColor),
-            ),
-          ),
-          const SizedBox(height: 12),
-          Flexible(
-            child: Container(),
-            flex: 2,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                child: Text('don have account'),
-                padding: const EdgeInsets.symmetric(vertical: 8),
-              ),
-              GestureDetector(
-                onTap: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const SignupScreen(),
-                  ),
-                ),
-                child: Container(
-                  child: Text(
-                    'signup',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                ),
+                    ),
+                ],
               )
+              //transition to sign up
             ],
-          )
-          //transition to sign up
-        ],
-      ),
-    )));
+          ),
+        )));
   }
 }
