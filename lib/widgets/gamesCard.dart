a8de98d1befe8c04e881ea7cafadae79c0878fb9
yosi <yosihofman21@gmail.com>
2024-10-13 22:42:14 +0300

    TeamLinkHandler in games card

--- lib/widgets/gamesCard.dart
+++ lib/widgets/gamesCard.dart
@@ -4,6 +4,7 @@ import 'package:football/models/games.dart';
 import 'package:football/models/guesses.dart';
 import 'package:football/theme/colors.dart';
 import 'package:football/widgets/teamLInks.dart';
+
 import 'package:intl/intl.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
