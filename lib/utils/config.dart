dbce45c52888cba068a55922de3dee360ee594fa
yosi <yosihofman21@gmail.com>
2025-07-14 12:26:26 +0300

    package name and new service json

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,4 +1,7 @@
 // const String backendUrl = 'http://10.0.2.2:5000';
 // const String backendUrl = 'http://10.0.2.2:5000';
-const String serverClientId = '319642473794-269lu0hmmfsig13b52p9a127mjbdbrpb.apps.googleusercontent.com';
+//com.football
+const String serverClientIdOld = '319642473794-269lu0hmmfsig13b52p9a127mjbdbrpb.apps.googleusercontent.com';
+//yosi.football
+const String serverClientId = '319642473794-ka8qllifpa80b7mbta6mcojiq3n4f5v6.apps.googleusercontent.com';
 const String backendUrl = 'https://leagues.onrender.com';
